6ee5b64a8c75d730f9a00996ceb6380900a2aeeb
Gurnek Mokha <gurnekmokha96@gmail.com>
2017-09-14 13:26:12 -0500

    Moving algorithms into a pandas DataFrame.

--- solver.py
+++ solver.py
@@ -3,58 +3,37 @@ this file will host the algorithm for reading and solving the cube matrix
 and generating the solving sequence for the cube.readsol function to use.
 '''
 import numpy as np
+import pandas as pd
+list_of_names = ['b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l']
+preDataframe = {
+    'moveset' : pd.Series([
+        'RuLuurURuurlU',
+        'frURUrufuFRfUff',
+        'ffruBfflDldll',
+        'FufUFulfRfrfLff',
+        'FruRfflDldllF',
+        'rDFrURfRBRbrd',
+        'uubUfuFBuufuF',
+        'uuluLuFuluLUf',
+        'RUrrfRBDrdFRb',
+        'ufUFrfURUruuFRU',
+    ], index = list_of_names),
 
-corner_alg = {
-    'b' : {
-        'moveset' : 'RuLuurURuurlU',
-        'delta' : ((0, 0, 0), (0, 0, 2), (0, 0, 1), (0, 1, 0), (2, 0, 0), (3, 0, 0), (2, 0, 1), (3, 0, 1), (3, 0, 2), (4, 0, 2))
-    },
-
-    'c' : {
-        'moveset' : 'frURUrufuFRfUff',
-        'delta' : ((0, 0, 0), (0, 2, 2), (0, 1, 0), (0, 0, 1), (1, 0, 2), (3, 0, 2), (2, 0, 0), (4, 0, 0), (2, 0, 2), (3, 0, 2))
-    },
-
-    'd' : {
-        'moveset' : 'ffruBfflDldll',
-        'delta' : ((0, 0, 0), (0, 2, 0), (0, 1, 0), (0, 0, 1), (1, 0, 0), (2, 0, 0), (2, 0, 1), (3, 0, 1), (2, 0, 2), (3, 0, 2))
-    },
-
-    'f' : {
-        'moveset' : 'FufUFulfRfrfLff',
-        'delta' : ((0, 0, 0), (2, 0, 2), (0, 1, 0), (0, 0, 1), (0, 2, 0), (2, 0, 0), (1, 0, 0), (3, 0, 2), (2, 0, 1), (3, 0, 1))
-    },
-
-    'g' : {
-        'moveset' : 'FruRfflDldllF',
-        'delta' : ((0, 0, 1), (0, 1, 0), (0, 0, 0), (2, 2, 2), (1, 2, 0), (2, 0, 0), (2, 0, 1), (3, 0, 1), (3, 0, 2), (5, 0, 0))
-    },
-
-    'h' : {
-        'moveset' : 'rDFrURfRBRbrd',
-        'delta' : ((0, 0, 0), (2, 2, 0), (0, 0, 1), (0, 1, 0), (2, 0, 0), (5, 2, 0), (2, 0, 1), (3, 0, 1), (3, 0, 2), (3, 2, 2))
-    },
-
-    'i' : {
-        'moveset' : 'uubUfuFBuufuF',
-        'delta' : ((0, 0, 0), (1, 0, 0), (0, 0, 1), (0, 1, 0), (0, 2, 0), (3, 0, 2), (2, 0, 1), (3, 0, 1), (2, 0, 0), (2, 0, 2))
-    },
-
-    'j' : {
-        'moveset' : 'uuluLuFuluLUf',
-        'delta' : ((0, 1, 0), (0, 0, 1), (0, 0, 0), (1, 0, 2), (0, 2, 2), (2, 0, 0), (2, 0, 1), (3, 0, 1), (3, 0, 2), (4, 0, 0))
-    },
-
-    'k' : {
-        'moveset' : 'RUrrfRBDrdFRb',
-        'delta' : ((0, 0, 0), (1, 2, 2), (0, 0, 1), (0, 1, 0), (2, 0, 1), (3, 0, 1), (2, 0, 0), (4, 2, 0), (3, 0, 2), (5, 0, 2))
-    },
-
-    'l' : {
-        'moveset' : 'ufUFrfURUruuFRU',
-        'delta' : ((0, 0, 1), (0, 1, 0), (0, 0, 0), (1, 2, 0), (2, 0, 1), (3, 0, 1), (2, 0, 0), (5, 0, 0), (2, 2, 2), (3, 0, 2))
-    }
+    'delta' : pd.Series([
+        '000 002 001 010 200 300 201 301 302 402',
+        '000 022 010 001 102 302 200 400 202 302',
+        '000 020 010 001 100 200 201 301 202 302',
+        '000 202 010 001 020 200 100 302 201 301',
+        '001 010 000 222 120 200 201 301 302 500',
+        '000 220 001 010 200 520 201 301 302 322',
+        '000 100 001 010 020 302 201 301 200 202',
+        '010 001 000 102 022 200 201 301 302 400',
+        '000 122 001 010 201 301 200 420 302 502',
+        '001 010 000 120 201 301 200 500 222 302'
+    ], index = list_of_names)
 }
+corner_alg = pd.DataFrame(preDataframe)
+
 edge_alg = {}
 move_string = ''
 def solve(matrix):
