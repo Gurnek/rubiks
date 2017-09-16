91dec190c3a2ee664b4432e79d98e8371c296933
Gurnek Mokha <gurnekmokha96@gmail.com>
2017-09-16 10:18:37 -0500

    Adding the remaining algorithms and adding the empty dataframe for the edge algorithm

--- solver.py
+++ solver.py
@@ -4,8 +4,8 @@ and generating the solving sequence for the cube.readsol function to use.
 '''
 import numpy as np
 import pandas as pd
-list_of_names = ['b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l']
-preDataframe = {
+list_of_names = ['b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 's', 't', 'u', 'v', 'w', 'x' ]
+preDataframe_c = {
     'moveset' : pd.Series([
         'RuLuurURuurlU',
         'frURUrufuFRfUff',
@@ -17,6 +17,17 @@ preDataframe = {
         'uuluLuFuluLUf',
         'RUrrfRBDrdFRb',
         'ufUFrfURUruuFRU',
+        'uulUFufuLuFuf',
+        'uuLuRUrluuRUr',
+        'udfLulFlblBLDLU',
+        'rURUrufuFRfUF',
+        'ufRFrfflfLfUruR',
+        'fdflFLFrFUfRD',
+        'rURfRBRbrdrDF',
+        'ufUFulfRfrfLf',
+        'ufRflfLfruFUf',
+        'UrrfRBDrdFRbR',
+        'uulUfRfrffUfuFL',
     ], index = list_of_names),
 
     'delta' : pd.Series([
@@ -29,12 +40,27 @@ preDataframe = {
         '000 100 001 010 020 302 201 301 200 202',
         '010 001 000 102 022 200 201 301 302 400',
         '000 122 001 010 201 301 200 420 302 502',
-        '001 010 000 120 201 301 200 500 222 302'
+        '001 010 000 120 201 301 200 500 222 302',
+        '001 010 000 400 022 302 200 102 201 301',
+        '001 010 000 402 002 200 201 301 300 302',
+        '001 010 000 422 200 320 201 301 302 522',
+        '001 010 000 420 122 302 200 502 201 301',
+        '010 001 000 300 002 302 201 301 200 402',
+        '010 001 000 322 200 220 201 301 302 520',
+        '010 001 000 320 200 522 201 301 302 422',
+        '010 001 000 500 120 302 200 222 201 301',
+        '010 001 000 502 122 200 201 301 302 420',
+        '010 001 000 522 200 422 201 301 302 320',
+        '010 001 000 520 201 301 200 322 220 302',
     ], index = list_of_names)
 }
-corner_alg = pd.DataFrame(preDataframe)
+preDataframe_e = {
+    'moveset' : pd.Series(),
+    'delta' : pd.Series()
+}
+corner_alg = pd.DataFrame(preDataframe_c)
 
-edge_alg = {}
+edge_alg = pd.DataFrame(preDataframe_e)
 move_string = ''
 def solve(matrix):
     iterator = np.nditer(matrix, flags = ['multi_index'])
